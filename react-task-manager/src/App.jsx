a798668597b8225a765c89ce993dde9dfac67cff
Nicole Mabano <archivalnicolel@gmail.com>
2026-09-22 20:41:07 +0800

    Update gitignore

--- react-task-manager/src/App.jsx
+++ react-task-manager/src/App.jsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [task, setTask] = useState("");
+ const [tasks, setTasks] = useState(() => {
+  const savedTasks = localStorage.getItem("tasks");
+  return savedTasks ? JSON.parse(savedTasks) : [];
+});
+ const [task, setTask] = useState("");
+ 
+ useEffect(() => {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}, [tasks]);
 
   const addTask = () => {
     if (task.trim() === "") {
